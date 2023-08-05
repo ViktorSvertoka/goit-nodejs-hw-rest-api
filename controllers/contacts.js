9788bf8220e04e0486922d6225335a7f618e25f8
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-08-05 21:22:44 +0300

    Fix(JS) Update code

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -33,12 +33,15 @@ const removeContact = async (req, res) => {
 };
 
 const updateContact = async (req, res) => {
-  if (Object.keys(req.body).length === 0) {
-    throw HttpError(400, 'missing fields');
-  }
   const { id } = req.params;
   const result = await contacts.updateContact(id, req.body);
-  res.status(200).json(result);
+
+  if (!result) {
+    throw HttpError(404, 'Not found');
+  }
+  res.json({
+    message: 'contact updated',
+  });
 };
 
 module.exports = {
