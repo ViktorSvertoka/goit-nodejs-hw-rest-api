d8a5edb0ba298b16d87b2bd6a7f730effeb8b582
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-08-08 23:22:19 +0300

    Update contact.js

--- models/contact.js
+++ models/contact.js
@@ -42,7 +42,7 @@ const addSchema = Joi.object({
 
 const updateFavoriteSchema = Joi.object({
   favorite: Joi.boolean()
-    .required()
+    .optional()
     .messages({ 'any.required': `Missing field favorite` }),
 });
 
