70c376523fc17dcce2ed35b6b3f90944e4f2b1b3
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-08-12 00:49:08 +0300

    Feat(Files) Finished HW 04

---
+++ controllers/auth.js
@@ -0,0 +1,83 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
+
+const { User } = require('../models/user');
+
+const HttpError = require('../helpers/HttpError');
+const ctrlWrapper = require('../helpers/ctrlWrapper');
+
+const { SECRET_KEY } = process.env;
+
+const register = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user) {
+    throw HttpError(409, 'Email in use');
+  }
+
+  const hashPassword = await bcrypt.hash(password, 10);
+  const newUser = await User.create({ ...req.body, password: hashPassword });
+
+  res.status(201).json({
+    user: {
+      email: newUser.email,
+      subscription: newUser.subscription,
+    },
+  });
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw HttpError(401, 'Email or password is wrong');
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    throw HttpError(401, 'Email or password is wrong');
+  }
+
+  const payload = { id: user._id };
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '23h' });
+
+  await User.findByIdAndUpdate(user._id, { token });
+
+  res.status(200).json({
+    token: token,
+    user: {
+      email: user.email,
+      subscription: user.subscription,
+    },
+  });
+};
+
+const current = async (req, res) => {
+  const { email, subscription } = req.user;
+  res.status(200).json({
+    email,
+    subscription,
+  });
+};
+
+const logout = async (req, res) => {
+  const { _id } = req.user;
+  const result = await User.findByIdAndUpdate(_id, { token: '' });
+
+  if (!result) {
+    throw HttpError(404, 'Not found');
+  }
+  res.status(204).json({});
+};
+
+module.exports = {
+  register: ctrlWrapper(register),
+  login: ctrlWrapper(login),
+  current: ctrlWrapper(current),
+  logout: ctrlWrapper(logout),
+};
--- models/contact.js
+++ models/contact.js
@@ -4,21 +4,33 @@ const Joi = require('joi');
 
 const handleMongooseError = require('../helpers/handleMongooseError');
 
+const nameRegex = /^[a-zA-Z\s-]+$/;
+const phoneRegex = /^\+?[0-9\s-]+$/;
+
 const contactSchema = new Schema(
   {
     name: {
       type: String,
       required: [true, 'Set name for contact'],
     },
+
     email: {
       type: String,
     },
+
     phone: {
       type: String,
     },
+
     favorite: {
       type: Boolean,
       default: false,
+    },
+
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'user',
+      required: true,
     },
   },
   { versionKey: false, timestamps: true }
@@ -27,7 +39,7 @@ const contactSchema = new Schema(
 contactSchema.post('save', handleMongooseError);
 
 const addSchema = Joi.object({
-  name: Joi.string().required().messages({
+  name: Joi.string().pattern(new RegExp(nameRegex)).required().messages({
     'any.required': `Missing required name field`,
   }),
 
@@ -35,7 +47,7 @@ const addSchema = Joi.object({
     'any.required': `Missing required email field`,
   }),
 
-  phone: Joi.string().required().messages({
+  phone: Joi.string().pattern(new RegExp(phoneRegex)).required().messages({
     'any.required': `Missing required phone field`,
   }),
 
